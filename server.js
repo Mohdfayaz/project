dcd7b805b5af93cd069739e6f8853205b371ac5a
Mohammed Fayaz z <mohammedfayazfn173148@gmail.com>
2025-02-22 19:20:30 +0530

    added files

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
-const mongoose = require('mongoose');
-const { MONGODB_URI, PORT } = require('./utils/config');
-const app = require('./app');
+const mongoose = require("mongoose");
+const { MONGODB_URI, PORT } = require("./utils/config");
+const app = require("./app");
 
-console.log('connecting to database......');
-mongoose.connect(MONGODB_URI)
-    .then(() => {
-        console.log('connected to database!');
-        
-        // Start the server
-        app.listen(PORT, () => {
-            console.log(`Server is running @ http://localhost:${PORT}`);
-        });
-    })
-    .catch((error) => {
-        console.log('Error connecting to database: ${error} ');
-    })
+console.log(`Connecting to the database...`);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log(`Connected to the database!`);
+
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server running @ http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`Error connecting to the database: ${error}`);
+  });
